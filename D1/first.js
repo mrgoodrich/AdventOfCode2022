a48fa1bed28453de8c9daf024ec6a8e867144c4d
Matt Goodrich <mgoodrich@foreflight.com>
2022-11-30 23:32:43 -0500

    .

---
+++ D1/first.js
@@ -0,0 +1,46 @@
+// ----------- PACKAGES -----------------------
+const fs = require('fs');
+const {all, any, contains, enumerate, filter, iter, map, partition, permutations, range, reduce, reduce_, sorted, sum, toArray, zip, zip3} = require('iter-tools');
+const {max, min} = require('itertools');
+// const Map = require('collections/map');
+const pycollections = require('pycollections');
+
+// ----------- INPUT SETUP --------------------
+const inFile = fs.readFileSync('inputs/input', 'utf8');
+
+// ----------- TESTING AND REGEX -----®---------
+const nonWhitespace = /\S+/g;
+const positiveOrNegativeNumber = /(-?)(\d+)/g;
+const numberAndWord = /(-?)(\d+) \S+/g;
+const entireLine = /([^\n]*)\n/gm
+
+const regex = entireLine; //      /()\n/gm;
+
+let input = [];
+let count = 0;
+
+// ----------- SOLUTION ------------------------
+let result;
+while((result = regex.exec(inFile)) !== null) {
+  const entireMatch = result[0]; // has new line char if entireLine
+  const firstGroup = result[1];
+  input.push(firstGroup);
+}
+
+// for (const ndx in input) {
+//
+// }
+
+// for (const v of input) {
+//   for (const v2 of input) {
+//
+//   }
+// }
+
+for (const v of input) {
+  console.log(v);
+}
+
+
+
+// ----------- GUESSES -------------------------
