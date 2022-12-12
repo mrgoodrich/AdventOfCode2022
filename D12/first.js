a8a12f77bc9e25e8f3e98be64299963817452ffe
Matt Goodrich <mgoodrich@foreflight.com>
2022-12-12 02:11:47 -0500

    ⛰️ Hill Climbing Algorithm 🥾 - 1: 6375, 2: 6749

---
+++ D12/first.js
@@ -0,0 +1,145 @@
+const fs = require('fs');
+eval(fs.readFileSync('../helpers.js')+'');
+const inFile = fs.readFileSync('inputs/input', 'utf8');
+
+const regex = entireLine; // /([^\n]*)\n/gm
+
+let input = [];
+// let inputCopy = [...input];
+
+let result;
+while((result = regex.exec(inFile)) !== null) {
+  const entireMatch = result[0]; // has new line char if entireLine
+  const firstGroup = result[1];
+  input.push(firstGroup);
+}
+
+// input = input[0];
+
+// let firstIter =
+// print(execPipe(input,
+//   // map(split),
+//
+// ));
+
+let startX = 0;
+let startY = 0;
+
+let alts = [];
+for (let row of input) {
+  let thisRow = [];
+  for (let v of row) {
+    switch (v) {
+      case 'S':
+        startX = row.indexOf(v);
+        startY = input.indexOf(row);
+        thisRow.push(0);
+        break;
+      case 'E':
+        thisRow.push(25)
+        break;
+      default:
+        thisRow.push(v.charCodeAt(0) - 97);
+        break;
+    }
+  }
+  alts.push(thisRow);
+}
+// print(alts)
+
+let bestPerSquare = {};
+
+let width = alts[0].length;
+let height = alts.length;
+
+let shortest = 100000;
+
+function findBest(x, y, stepCount, path, depth) {
+  // print('path: ')
+  // print(path)
+  //print(stepCount)
+  let newHeight = alts[y][x];
+  //print('new height ' + newHeight);
+  if (newHeight == 25) {
+    // print('found end! ' + stepCount);
+    // print(path)
+    let total = stepCount;
+    if (total < shortest) {
+      shortest = total;
+    }
+    return [total, path];
+  }
+  let possibles = [
+    [x, y - 1],
+    [x, y + 1],
+    [x - 1, y],
+    [x + 1, y]
+  ];
+  // print(possibles);
+  // exit(0)
+  let bestSub = 10000000;
+  let bestPath = {};
+  for (let next of possibles) {
+
+    //print('next: ' + next)
+    //print(bestPerSquare)
+    //print('checking ' + next[0] + ', ' + next[1]);
+    if (next[0] >= 0 && next[0] < width && next[1] >= 0 && next[1] < height) {
+      // print('thispath:')
+      // print(thisPath)
+      //print(alts.length)
+      //print('looking at ' + next)
+      let possibleNextAlt = alts[next[1]][next[0]];
+      if (!bestPerSquare[[next[0], next[1]]] || bestPerSquare[[next[0], next[1]]] > stepCount) {
+        if (possibleNextAlt <= newHeight + 1) {
+          bestPerSquare[[next[0], next[1]]] = stepCount;
+          // let thisPath = new Map(path);
+          // thisPath.set(next[0] + ',' + next[1], depth);
+          let res = findBest(next[0], next[1], stepCount + 1, path + '|' + next, depth + 1);
+          let total = res[0];
+          let totalBest = res[1];
+          if (total < bestSub) {
+            bestSub = total;
+            bestPath = totalBest;
+          }
+        }
+      }
+    }
+  }
+  return [bestSub, bestPath];
+}
+
+// let initVisited = new Map();
+// initVisited.set(startX + ',' + startY, 0);
+let best = findBest(startX, startY, 0, + startX + ',' + startY, 1);
+let bestVisited = best[1];
+// print(best)
+
+let bestRoute = new Set(bestVisited.split('|'));
+// print(best[1].size)
+
+// print(bestRoute.has([0,0]))
+
+for (let y = 0; y < input.length; y++) {
+  let thisLine = '';
+  for (let x = 0; x < input[0].length; x++) {
+    if (bestRoute.has(x + ',' + y)) {
+      // if (bestRoute.get(x + ',' + y) >= 10) {
+      //   thisLine += '*';
+      // } else {
+      //   thisLine += bestRoute.get(x + ',' + y);
+      // }
+      thisLine += '*';
+    } else {
+      thisLine += input[y][x];
+    }
+  }
+  console.log(thisLine);
+}
+print(shortest)
+
+
+// ----------- GUESSES -------------------------
+
+
+//
